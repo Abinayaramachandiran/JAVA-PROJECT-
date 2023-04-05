4b683e106f94dd9e4e91cc459f532c07c0422b83
Admin <Admin@DESKTOP-CA718VE>
2023-04-05 07:38:38 -0700

    shopping management

---
+++ src/main/java/com/tns/customer_service/CustomerServicesRepository.java
@@ -0,0 +1,7 @@
+package com.tns.customer_service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerServicesRepository extends JpaRepository<Customer,Integer> {
+
+}
